390e3a746a8f98228073b62fc291203abc8bf22e
Aleksandr Onishchuk <aleksandr200689@yandex.ru>
2026-04-16 13:26:08 +0800

    устранение замечаний по первому этапу

--- core/main-service/src/main/java/ru/practicum/events/mapper/LocationMapper.java
+++ core/main-service/src/main/java/ru/practicum/events/mapper/LocationMapper.java
@@ -1,18 +1,12 @@
 package ru.practicum.events.mapper;
 
 import lombok.experimental.UtilityClass;
-import org.mapstruct.factory.Mappers;
 import ru.practicum.events.dto.LocationDto;
 import ru.practicum.events.model.Location;
 
 @UtilityClass
 public class LocationMapper {
-    private static final LocationMapper INSTANCE = Mappers.getMapper(LocationMapper.class);
-
     public Location toLocation(LocationDto dto) {
-        if (dto == null) {
-            return null;
-        }
         Location location = new Location();
         location.setLat(dto.getLat());
         location.setLon(dto.getLon());
@@ -20,20 +14,9 @@ public class LocationMapper {
     }
 
     public LocationDto toLocationDto(Location location) {
-        if (location == null) {
-            return null;
-        }
         return LocationDto.builder()
                 .lat(location.getLat())
                 .lon(location.getLon())
                 .build();
-    }
-
-    public static LocationDto toLocationDtoStatic(Location location) {
-        return INSTANCE.toLocationDto(location);
-    }
-
-    public static Location toLocationStatic(LocationDto dto) {
-        return INSTANCE.toLocation(dto);
     }
 }
